5732b622b65ce58c2eb36efb226000a9a24235cf
Mena Ehab <commanderyt123@gmail.com>
2025-08-31 13:04:06 +0300

    feat: add layout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
-
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
 function App() {
 
   return (
-    <>
-
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+      </Route>
+    </Routes>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,28 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from '@mui/material/styles'
+import { createTheme } from '@mui/material/styles';
+import { blue } from '@mui/material/colors';
+import { grey } from '@mui/material/colors';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: blue[500],
+    },
+    secondary: {
+      main: grey[500],
+    },
+  },
+});
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
